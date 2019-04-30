59855a18f0b7f888c32bac7d6ff21d50d967a750
liao <liaomovie2@gmail.com>
2019-05-01 00:25:20 +0800

    first commit

---
+++ clean_explore.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+#### Get Date of Chinese New Year ####
+library(seasonal)
+data(holiday) # cny: date of chinese New year
+new_year <- function(year) {
+  pat <- paste0('^', as.character(year))
+  new_year <- seasonal::cny[grepl(pat, seasonal::cny)]
+  return(as.Date(new_year, '%Y-%m-%d'))
+}
+
+#### Data Import ####
+data <- readr::read_csv('2005-2017.csv')
+colnames(data) <- c('date', 'id', 'station', 'num_in', 'num_out')
+
+data <- data %>%
+  mutate(date = as.Date(as.character(date), '%Y%m%d'))
+
+#### Plotting ####
+
+## Function: Filter year and stations
+filter_sta_y <- function(data, sta = '台北', year = 2005) {
+  data %>% filter(station %in% sta) %>%
+    filter(year(date) == year )
+}
+
+## Function: Plot time series data of 1 year (daily) for many stations
+plot_daily <- function(data, print = TRUE) {
+  new_year_date <- new_year(lubridate::year(data$date[1]))
+  
+  pl <- ggplot(data = data) +
+    geom_line(aes(
+      x = date, 
+      y = (num_in - num_out)/(num_in + num_out),
+      color = station), 
+      size = 0.3) +
+    geom_vline(xintercept = as.numeric(new_year_date), 
+               linetype = 4, size = 0.2) +
+    geom_vline(xintercept = as.numeric(new_year_date - days(1)), 
+               linetype = 3, size = 0.2) +
+    labs(y = '(進站 - 出站) / 總人數') +
+    theme(axis.title.y = element_text(size = 10))
+  if (print) print(pl)
+  return(pl)
+}
+
